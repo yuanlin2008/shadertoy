04bed3666bfccc42a84f6904e2aa92aef4869cc8
yuanlin2008 <yuanlin2008@hotmail.com>
2024-10-10 17:34:09 +0800

    123

--- common.glsl
+++ common.glsl
@@ -1,27 +1,22 @@
 /** Screen to world coord. */
-vec2 screenToWorld(in vec2 fragCoord){
+vec2 screenToWorld(in vec2 fragCoord) {
     vec2 pos = fragCoord / iResolution.xy - 0.5;
-    pos.x *= iResolution.x/iResolution.y;
-    return pos*2.0;
+    pos.x *= iResolution.x / iResolution.y;
+    return pos * 2.0;
 }
 
-vec2 worldToLocal(in vec2 translation, in float rotation, in vec2 pos){
+vec2 worldToLocal(in vec2 translation, in float rotation, in vec2 pos) {
     pos -= translation;
     float sin = sin(rotation);
     float cos = cos(rotation);
-    return mat2(
-        cos, sin,
-        -sin, cos
-    ) * pos;
+    return mat2(cos, sin, -sin, cos) * pos;
 }
 
-float inCircle(in vec2 center, in float radius, float edge, in vec2 pos){
-    return 1.-smoothstep(radius, radius + edge, length(pos - center));
+float distToCircle(in vec2 center, in float radius, in vec2 pos) {
+    return length(pos - center) - radius;
 }
 
-float inRect(in vec2 center, in float rot, in vec2 size, float edge, in vec2 pos){
-    pos = worldToLocal(center, rot, pos);
-    vec2 d = vec2(1) - 
-        smoothstep(size, size + vec2(edge), abs(pos));
-    return d.x*d.y;
+float distToRect(in vec2 center, in float rot, in vec2 size, in vec2 pos) {
+    pos = abs(worldToLocal(center, rot, pos))-size;
+    return max(pos.x, pos.y);
 }
--- test.glsl
+++ test.glsl
@@ -3,7 +3,7 @@
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 pos = screenToWorld(fragCoord);
     float clr = 0.0;
-    clr += inRect(vec2(0.,0.),iTime*1., vec2(5.4, 0.0), 0.04, pos);
-    clr += inCircle(vec2(0.0,0.0), 0.1, 0.3, pos);
+    clr += smoothstep(0., 0.1, 0.1-distToRect(vec2(0.,0.),iTime, vec2(5.4, 0.01), pos));
+    clr += smoothstep(0., 0.1, 0.1-distToCircle(vec2(0.0,0.0), 0.2, pos));
     fragColor = vec4(clr, clr, clr, 1);
 }
