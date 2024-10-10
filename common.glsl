cd6b5fb5b981ab50ed3f0e9888627b756c9b795e
yuanlin2008 <yuanlin2008@hotmail.com>
2024-10-10 16:41:43 +0800

    123

--- common.glsl
+++ common.glsl
@@ -6,21 +6,21 @@ vec2 screenToWorld(in vec2 fragCoord){
 }
 
 vec2 worldToLocal(in vec2 translation, in float rotation, in vec2 pos){
+    pos -= translation;
     float sin = sin(rotation);
     float cos = cos(rotation);
-    return (inverse(mat3(
-        vec3(cos, -sin, 0),
-        vec3(sin, cos, 0),
-        vec3(translation.xy, 1)
-    )) * vec3(pos,1)).xy;
+    return mat2(
+        cos, sin,
+        -sin, cos
+    ) * pos;
 }
 
-float checkCircle(in vec2 center, in float radius, in vec2 pos){
-    return step(length(pos - center), radius);
+float SDFCircle(in vec2 center, in float radius, float edge, in vec2 pos){
+    return 1.-smoothstep(radius, radius + edge, length(pos - center));
 }
 
-float checkRectangle(in vec2 center, in float rot, in vec2 size, in vec2 pos){
+float SDFRect(in vec2 center, in float rot, in vec2 size, float edge, in vec2 pos){
     pos = worldToLocal(center, rot, pos);
-    vec2 d = step(abs(pos - center), size);
+    vec2 d = vec2(1) - smoothstep(size, size + vec2(edge), abs(pos));
     return d.x*d.y;
 }
--- test.glsl
+++ test.glsl
@@ -2,7 +2,8 @@
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 pos = screenToWorld(fragCoord);
-    float clr = checkRectangle(vec2(0.0,0.0),1.0, vec2(0.5, 0.8), pos);
+    float clr = SDFRect(vec2(0.8,0.4),iTime, vec2(0.4, 0.2), 0.1, pos);
+    //float clr = SDFCircle(vec2(0.8,0.4), 0.2, 0.3, pos);
     // Output to screen
     fragColor = vec4(clr, clr, clr, 1);
 }
