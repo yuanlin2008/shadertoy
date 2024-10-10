2cc9d889ff607940a1892e5884e7c2aa0ef5c464
yuanlin2008 <yuanlin2008@hotmail.com>
2024-10-10 17:00:23 +0800

    123

--- test.glsl
+++ test.glsl
@@ -3,7 +3,7 @@
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 pos = screenToWorld(fragCoord);
     float clr = 0.0;
-    clr += inCircle(vec2(0.0,0.0), 0.2, 0.1, pos);
-    clr += inRect(vec2(0.8,0.4),iTime, vec2(5.4, 0.0), 0.02, pos);
+    clr += inRect(vec2(0.,0.),iTime*1., vec2(5.4, 0.0), 0.04, pos);
+    clr += inCircle(vec2(0.0,0.0), 0.1, 0.3, pos);
     fragColor = vec4(clr, clr, clr, 1);
 }
